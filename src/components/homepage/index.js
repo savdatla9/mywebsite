fea3791d310ff535040f3b19efc84545c3d96b29
Sai Akhil Varma Datla <varmaakhil27@gmail.com>
2024-08-23 18:04:38 +0530

    info 3d ui part done

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { ScrollControls } from '@react-three/drei';
+import { ScrollControls, Scroll, SoftShadows } from '@react-three/drei';
 
 import Pages from './pages';
 
@@ -13,16 +13,25 @@ function Home() {
         <>
             <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }} style={{width: '100%', height: '100vh'}}>
                 <color attach="background" args={["#282c34"]} />
+
+                <ambientLight intensity={1} color={'#cccccc'} />
+
                 <fog attach="fog" args={["#f5f3ee", 10, 50]} />
-                <ScrollControls
-                    pages={config.sections.length}
-                    damping={0.1}
-                    maxSpeed={0.2}
-                >
-                    <group position-y={-1}>
+
+              
+
+                <ScrollControls pages={config.sections.length}>                    
+                    <Scroll>
                         <Pages />
-                    </group>
+                    </Scroll>
                 </ScrollControls>
+                
+                <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1.01, 0]} receiveShadow>
+                    <planeGeometry args={[10, 10, 1, 1]} />
+                    <shadowMaterial transparent opacity={0.75} />
+                </mesh>
+
+                <SoftShadows size={40} samples={16} />
             </Canvas>
         </>
     );
--- src/components/homepage/pages.js
+++ src/components/homepage/pages.js
@@ -1,15 +1,153 @@
-import { Environment } from "@react-three/drei";
+import React, { useRef } from 'react';
+
+import { useThree } from "@react-three/fiber";
+import { Environment, Float, Text3D, useIntersect, MeshWobbleMaterial, useTexture, useGLTF, OrbitControls } from "@react-three/drei";
+
+
+export const SectionTitle = ({ color, children, ...props }) => {
+    return (
+        <Text3D font={"font/Inter_Bold.json"} size={0.3} {...props}>
+            {children}
+            <meshPhysicalMaterial color={color ? color : 'white'} />
+        </Text3D>
+    );
+};
+
+export const Item = ({ position, children }) => {
+    const visible = useRef();
+    
+    const ref = useIntersect((isVisible) => (visible.current = isVisible));
+  
+    return (
+        <group ref={ref} position={position}>
+            {children}
+        </group>
+    );
+};
 
 const Pages = () => {
+    const { height, width } = useThree((state) => state.viewport);
+
+    const emoji = useTexture('/profile.jpg');
+
+    const ReactLogo = (props) => {
+        const gltf = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/react-logo/model.gltf');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
+    const Screen = (props) => {
+        const gltf = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
+    const Mobile = (props) => {
+        const gltf = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
+    const BackNode = (props) => {
+        const gltf = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/node/model.gltf');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
+    const UnrealEngineLogo = (props) => {
+        const gltf = useGLTF('/models/unreal_engine_logo.glb');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
+    const VRSet = (props) => {
+        const gltf = useGLTF('/models/vr_headset.glb');
+
+        return(
+            <mesh {...props}>
+                <primitive object={gltf.scene} />
+            </mesh>
+        );
+    };
+
     return (
         <>
             <Environment preset="sunset" />
-            <mesh>
-                <boxGeometry />
-                <meshStandardMaterial color="white" />
-            </mesh>
+
+            {/* <OrbitControls enableRotate={true} /> */}
+
+            <pointLight color="white" position={[8, -25, 5]} intensity={20} />
+                
+            <pointLight color='silver' position={[0, -height * 2.25, 5]} intensity={10} />
+
+            <Item position={[0, 0, 0]}>
+                <Float intensity={1.5}>
+                    <group>
+                        <mesh scale={0.25} position={[-0.5, 0.65, 0]}>
+                            <octahedronGeometry />
+                            <MeshWobbleMaterial wireframe color={'#ffb900'} />
+                        </mesh>
+
+                        <UnrealEngineLogo scale={0.001} position={[-0.32, 0.2, -0.48]} rotation={[0, 0, 0]} />
+
+                        <ReactLogo scale={0.15} position={[-0.25, -0.1, -0.4]} rotation={[0, 0, Math.PI/2]} />
+
+                        <Screen position={[-0.5, -0.513, 0]} scale={0.4} rotation={[0, 0, 0]} />
+                    
+                        <Mobile position={[0.35, -0.15, 0]} scale={0.15} rotation={[0, 0, 0]} />
+
+                        <VRSet position={[-1.4, -0.35, 0]} scale={0.05} rotation={[0, -Math.PI/2, 0]} />
+
+                        <BackNode position={[-0.85, 0.15, -0.4]} scale={0.15} rotation={[0, 0, 0]} />
+                    </group>
+                </Float>
+
+                <mesh position={[width/3, 0.8, 0.15]} scale={0.45}>
+                    <circleGeometry />
+                    <MeshWobbleMaterial color="lightyellow" map={emoji}  />
+                </mesh>
+            </Item>
+                    
+            <Item position={[-width / 5, -height * 0.985, -2]}>
+                <SectionTitle position-x={0.5}>SKILLS</SectionTitle>
+            </Item>
+
+            <Item position={[-width / 6, -height * 1.985, -2]}>
+                <SectionTitle position-x={0.5} color={'springgreen'}>PROJECTS</SectionTitle>
+            </Item>
+
+            <Item position={[-1.85, -height * 3, -2]}>
+                <SectionTitle position-x={0} color={'dodgerblue'} rotation={[0, 0.15, 0]}>CONTACT</SectionTitle>
+            </Item>
         </>
     );
 };
 
 export default Pages;
+
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/react-logo/model.gltf');
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf');
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/node/model.gltf');
+useGLTF.preload('/models/unreal_engine_logot.glb');
+useGLTF.preload('/models/vr_headset.glb');
